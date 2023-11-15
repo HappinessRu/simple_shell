26407784ccb2fc1d5cf84f8c27039cc57cb1c898
Kundai <kundaimuchemwa456@gmail.com>
2023-11-15 15:02:00 -0800

    input reader

---
+++ read_input_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_line - input string.
+ * @i_eof: value of getline function
+ * Return: input str
+ */
+char *read_line(int *i_eof)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&input, &bufsize, stdin);
+
+	return (input);
+}
