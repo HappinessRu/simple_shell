a93cce0660a012c8c26cbf37200ad779f6ac95a8
Kundai <kundaimuchemwa456@gmail.com>
2023-11-15 14:40:05 -0800

    builtin

---
+++ disp_builtin.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_builtin - builtin that pais the command in the arg
+ * @cmd: command
+ * Return: function pointer of the builtin command
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int z;
+
+	for (z = 0; builtin[z].name; z++)
+	{
+		if (_strcmp(builtin[z].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[z].f);
+}
